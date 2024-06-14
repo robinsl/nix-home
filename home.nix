9db44cb855de4283131d52eca7aaed05ef7b460d
Robin Simard Lamarre <robinlamarre@gmail.com>
2024-06-14 15:31:58 -0400

    tab size vim conf

--- home.nix
+++ home.nix
@@ -1,51 +1,53 @@
 {config, pkgs, ...}: {
-  fonts.fontconfig.enable = true;
+    fonts.fontconfig.enable = true;
 
-	home = {
-		username = "robin";
-		homeDirectory = "/home/robin";
-		stateVersion = "24.05";
+    home = {
+        username = "robin";
+        homeDirectory = "/home/robin";
+        stateVersion = "24.05";
 
-		packages = with pkgs; [
-			neofetch
-      fzf
-      kitty-themes
-      (pkgs.nerdfonts.override { fonts = [ "Hack" "DroidSansMono" ]; })
-		];
+        packages = with pkgs; [
+            neofetch
+                fzf
+                kitty-themes
+                (pkgs.nerdfonts.override { 
+                 fonts = [ "Hack" "DroidSansMono" ]; 
+                 })
+        ];
 
-		file."./.config/nvim/" = {
-			source = ./nvim;
-			recursive = true;
-		};
+        file."./.config/nvim/" = {
+            source = ./nvim;
+            recursive = true;
+        };
 
-    file."./.config/alacritty/alacritty.toml" = {
-      source = ./alacritty.toml;
+        file."./.config/alacritty/alacritty.toml" = {
+            source = ./alacritty.toml;
+        };
+
+        file."./.config/kitty/kitty.conf" = {
+            source = ./kitty.conf;
+        };
+
     };
 
-    file."./.config/kitty/kitty.conf" = {
-      source = ./kitty.conf;
+    programs.bash = {
+        enable = true;
+        shellAliases = {
+            rebuild = "sudo nixos-rebuild switch";
+            rehome = "home-manager switch";
+        };
+
     };
 
-	};
- 
-	programs.bash = {
-		enable = true;
-		shellAliases = {
-			rebuild = "sudo nixos-rebuild switch";
-			rehome = "home-manager switch";
-		};
-    
-	};
-
-	programs.neovim = {
-		enable = true;
-		defaultEditor = true;
-		viAlias = true;
-		vimAlias = true;
-		vimdiffAlias = true;
-		plugins = with pkgs.vimPlugins; [
-			dracula-vim
-			oil-nvim
-		];
-	};
-}
+    programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
+        vimdiffAlias = true;
+        plugins = with pkgs.vimPlugins; [
+            dracula-vim
+                oil-nvim
+        ];
+    };
+                     }
--- nvim/lua/configs/settings.lua
+++ nvim/lua/configs/settings.lua
@@ -2,3 +2,6 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 vim.opt.scrolloff = 8
+
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
